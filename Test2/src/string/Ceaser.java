c2d9a019c52c7cc8479bb7e6469ef97250852614
wowa_ <wd.zakrevskyy@icloud.com>
2020-02-12 21:03:26 +0100

    - ceaser example

---
+++ Test2/src/string/Ceaser.java
@@ -0,0 +1,83 @@
+package string;
+
+public class Ceaser {
+
+	public static void main(String[] args) {
+		String test = "baba";
+		System.out.println("text : " + test);
+		char[] clearText = test.toCharArray();
+		
+		// TODO homework
+		int key = 24;
+
+		char[] encrypted = new char[clearText.length];
+
+		for (int index = 0; index < clearText.length; index++) {
+			encrypted[index] = encryptChar(key, clearText[index]);
+		}
+		System.out.println("encrypted : " + new String(encrypted));
+
+		for (int index = 0; index < encrypted.length; index++) {
+			System.out.print(decryptChar(key, encrypted[index]));
+		}
+	}
+
+	static char decryptChar(int key, char ch) {
+		int index = getIndexTo(ch);
+		// TODO homework
+		//if(){}
+		//
+		//
+		int value = Math.abs((key - index)) % 25;
+
+		return getCharTo(value);
+	}
+
+	static char encryptChar(int key, char ch) {
+		int index = getIndexTo(ch);
+		int value = (key + index) % 25;
+
+		return getCharTo(value);
+	}
+
+	static int getIndexTo(char ch) {
+		char[] alphabet = getAlphabet();
+		for (int index = 0; index < alphabet.length; index++) {
+			if (ch == alphabet[index]) {
+				return index;
+			}
+		}
+		return -1;
+	}
+
+	static char getCharTo(int index) {
+		char[] alphabet = getAlphabet();
+
+		// TODO possible solution
+//		return alphabet[index];
+
+		for (int idx = 0; idx < alphabet.length; idx++) {
+			if (idx == index) {
+				return alphabet[idx];
+			}
+		}
+		return '~';
+	}
+
+	static char[] getAlphabet() {
+		// 1d char array
+		char[] alphabet = new char[26];
+
+		int counter = 97;
+		for (int index = 0; index < alphabet.length; index++) {
+//			System.out.println("set index to " + index);
+			if (counter >= 97 && counter <= 122) {
+//				System.out.println("set counter " + counter + " char is " + (char) counter);
+				alphabet[index] = (char) counter;
+			}
+			counter++;
+		}
+
+		return alphabet;
+	}
+}
